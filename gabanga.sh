8aa639cc4bff2569d4e6e417927b6439a1d3601a
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-24 10:48:20 +0300

    Removed unused info.

--- gabanga.sh
+++ gabanga.sh
@@ -1,6 +1,6 @@
 Yes or no. What you chouse?
 New life, new day. It's your life and I know be everything good.
-<<<<<<< HEAD
+
 I knew what it's happened. But I don't know so very fast. It's
 unbitteble.
 I knew what it's happened. But I don't know so very fast. It's
