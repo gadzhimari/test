0f045d1372b1d04b5e3cae41476ed6fcf9a3a2c7
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-24 10:48:45 +0300

    Add new message.

--- gabanga.sh
+++ gabanga.sh
@@ -12,6 +12,16 @@ I knew what it's happened. But I don't know so very fast. It's
 Yes or no. What you chouse?
 New life, new day. It's your life and I know be everything good.
 I knew what it's happened. But I don't know so very fast. It's
-=======
+
 This is what I want to say.
 What the fuck. I knew it, but still hope for you ability to think.
+
+Damn, she so hot
+And I know it before met
+What I'm gonna to do
+I don't know but something special for her
+
+Fool
+And I know it before met
+What I'm gonna to do
+I don't know but something special for her
