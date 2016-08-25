06a6bdb5d8592314bd83d69f5c8aebbf1d05e998
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-25 20:47:27 +0300

    Add new alafaria.

--- alafar.rb
+++ alafar.rb
@@ -2,15 +2,16 @@ WTF
 FAQ
 In the middle of the night
 Andrew Rayell has been suspended
+
+Add new alafaria.
+Alafaria is new land.
+
+In the middle of the night
+Andrew Rayell has been suspended
 <<<<<<< HEAD
-=======
 Alafaria is new land.
 
 In the middle of the night
 Andrew Rayell has been suspended
-Alafaria is new land.
 
-In the middle of the night
-Andrew Rayell has been suspended
->>>>>>> 418be3f... Barbarian.
 Alafaria is new land.
