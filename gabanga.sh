f8d295424d77a185dab9962af725a40f0be4eeab
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-25 21:23:50 +0300

    Ability choice.

--- gabanga.sh
+++ gabanga.sh
@@ -3,8 +3,3 @@ New life, new day. It's your life and I know be everything good.
 
 I knew what it's happened. But I don't know so very fast. It's
 unbitteble.
-I knew what it's happened. But I don't know so very fast. It's
-unbitteble.
-New life, new day. It's your life and I know be everything good.
-Yes or no. What you chouse?
-New life, new day. It's your life and I know be everything good.
