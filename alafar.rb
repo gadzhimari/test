08b5710253f08bfaf128d6c5a3c25977ebdcc120
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-25 10:52:57 +0300

    Delete alafaria.

--- alafar.rb
+++ alafar.rb
@@ -1,11 +1,1 @@
 WTF
-FAQ
-In the middle of the night
-Andrew Rayell has been suspended
-Alafaria is new land.
-
-Andrew Rayell has been suspended
-Alafaria is new land.
-
-Andrew Rayell has been suspended
-Alafaria is new land.
