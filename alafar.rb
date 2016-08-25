764e01f5d0d2f46c2de84c2c003984d653e2a7d2
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-25 09:52:02 +0300

    Added new land.

--- alafar.rb
+++ alafar.rb
@@ -3,3 +3,9 @@ FAQ
 In the middle of the night
 Andrew Rayell has been suspended
 Alafaria is new land.
+
+Andrew Rayell has been suspended
+Alafaria is new land.
+
+Andrew Rayell has been suspended
+Alafaria is new land.
