54e451ff6fa54a2cc97fd1048fe1261ba296a357
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-25 11:37:59 +0300

    Added new file.

--- gabanga.sh
+++ gabanga.sh
@@ -8,7 +8,3 @@ unbitteble.
 New life, new day. It's your life and I know be everything good.
 Yes or no. What you chouse?
 New life, new day. It's your life and I know be everything good.
-I knew what it's happened. But I don't know so very fast. It's
-Yes or no. What you chouse?
-New life, new day. It's your life and I know be everything good.
-I knew what it's happened. But I don't know so very fast. It's
