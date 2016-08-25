c40817068b27c3cf997883b3ac4f67ef42ef767b
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-08-25 10:57:03 +0300

    Added new day.

--- gabanga.sh
+++ gabanga.sh
@@ -12,22 +12,3 @@ I knew what it's happened. But I don't know so very fast. It's
 Yes or no. What you chouse?
 New life, new day. It's your life and I know be everything good.
 I knew what it's happened. But I don't know so very fast. It's
-
-This is what I want to say.
-What the fuck. I knew it, but still hope for you ability to think.
-
-Damn, she so hot
-And I know it before met
-What I'm gonna to do
-I don't know but something special for her
-
-Fool
-And I know it before met
-What I'm gonna to do
-I don't know but something special for her
-
-I knew what it's happened. But I don't know so very fast. It's
-unbitteble.
-New life, new day. It's your life and I know be everything good.
-
-Buenos dios
